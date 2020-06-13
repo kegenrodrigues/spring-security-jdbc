bf3497c649d2815a81af1eebc4057f64a79c1aaa
Kegen Rodrigues <kegenrodrigues95@gmail.com>
2020-06-13 17:59:32 +0530

    Use Jdbc connection

--- src/main/java/com/coders/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/coders/springsecurity/demo/config/DemoAppConfig.java
@@ -1,19 +1,37 @@
 package com.coders.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.coders.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
 
-	//define a bean for viewResolver
+	//setup variable to hold the properties
+	@Autowired
+	private Environment env;
 	
+	//setup a logger for diagnostics
+	Logger logger = Logger.getLogger(getClass().getName());
+	
+	
+	//define a bean for viewResolver
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -23,4 +41,45 @@ public class DemoAppConfig {
 		return viewResolver;
 	}
 	
+	//define a bean for our security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		//create connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		
+		//set the jdbc driver class
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		//log the connection props
+		logger.info("---->Driver class: "+env.getProperty("jdbc.driver"));
+		logger.info("---->Driver url: "+env.getProperty("jdbc.url"));
+		logger.info("---->Driver username: "+env.getProperty("jdbc.user"));
+		
+		//set database connection props
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		//set connection pool props
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		
+		return securityDataSource;
+		 
+	}	
+	
+	private int getIntProperty(String propName) {
+		String propVal = env.getProperty(propName);
+		int intPropVal = Integer.parseInt(propVal);
+		return intPropVal;
+	}	
 }
